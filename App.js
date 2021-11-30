8cf369cf438a41116570e3e583c1909b7d27176c
bluefoxmac <flori84@outlook.fr>
2021-11-30 13:38:44 +0100

    add modules

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Dayleats</Text>
+      <Text style={styles.emoji}>🛒</Text>
+      <Text style={styles.version}>V.0.1</Text>
+      <Button title="Se connecter"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "white",
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 60,
+    color: "black",
+    fontWeight: "bold",
+  },
+  emoji: {
+    fontSize: 50,
+  },
+  version: {
+    position: 'absolute',
+    fontSize: 10,
+    color: "grey",
+    paddingTop: 750,
   },
 });
